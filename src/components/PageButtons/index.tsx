b401f2fcd1584c404aa9cb889d1b74315c727e01
wspawned <wspawned@gmail.com>
2022-12-30 08:19:25 +0300

    Page buttons change position upon scroll

--- src/components/PageButtons/index.tsx
+++ src/components/PageButtons/index.tsx
@@ -1,24 +1,67 @@
 import './style.css';
 import { useNavigate } from "react-router-dom";
-
+import {useEffect, useState} from "react";
+//@ts-ignore
 const PageButtons:React.FC<{paramsPage:number, totalPages:number, paramsCategory:string}> = ({paramsPage, totalPages, paramsCategory}) => {
   const navigate = useNavigate();
+  const [activePageButtonsClass, setActivePageButtonsClass] = useState("page-buttons");
+
+  let buttonClasses = [
+    "Prev Page",
+    "Next Page",
+  ]
+  
+  const handleIntersectionObserver = () => {
+    //define options
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    }
+    //define targets
+    //@ts-ignore
+    const targets = document.getElementById('page-buttons').querySelectorAll('button')
+    //define the observer
+    //@ts-ignore
+    const activeClass = (target) => {
+      const buttonObserver = new IntersectionObserver((buttonClasses, observer) => {
+        buttonClasses.forEach(buttonClass => {
+          console.log(buttonClass)
+          if(buttonClass.isIntersecting) {
+            setActivePageButtonsClass("page-buttons-distributed");
+            // observer.disconnect;
+          } else {setActivePageButtonsClass("page-buttons");}
+        })
+      }, options)
+      buttonObserver.observe(target)
+    }
+    //loop trough the targets
+    targets.forEach(activeClass)
+  };
+
+  useEffect(()=>{
+    handleIntersectionObserver();
+  })
 
   return (
-    <div className='page-buttons'>
+    <div 
+    // className='page-buttons'
+    className={activePageButtonsClass}
+    id='page-buttons'
+    >
     {paramsPage>1 && (<button
     onClick={()=> {
       const newPageNumber =  Number(paramsPage) -1 ;
       navigate(`/?category=${paramsCategory}&page=${newPageNumber}`);
     } }
-    >{`${paramsPage -1} <= ${paramsPage}`}</button>)}
+    >{`Prev Page ${paramsPage -1} <= ${paramsPage}`}</button>)}
 
     {paramsPage<totalPages && (<button
     onClick={()=> {
       const newPageNumber =  Number(paramsPage) +1 ;
       navigate(`/?category=${paramsCategory}&page=${newPageNumber}`);
     } }
-    >{`${paramsPage} => ${paramsPage +1}`}</button>)}  
+    >{`Next Page ${paramsPage} => ${paramsPage +1}`}</button>)}  
   </div>
   )
 };
